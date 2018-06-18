1137975bf31246fba5c737961e77e35f430e0606
Zubetsandriy <zubets.andriy@gmail.com>
2018-06-18 12:20:03 +0300

    some

--- js/script.js
+++ js/script.js
@@ -12,3 +12,30 @@ if (document.querySelector('.sandwich-right-bar .nav-toggle')) {
 		}
 	}
 }
+if(document.querySelector('.main .wrap .left .slider')) {
+	person = document.querySelectorAll('.main .wrap .left .slider .item');
+    slideWrapper = document.querySelector('.main .wrap .left .slider .wrapper');
+    windowWidth = document.documentElement.clientWidth;
+    step = Math.round(slideWrapper.offsetWidth / person.length);
+    rightBut = document.querySelector('.main .wrap .left .slider .right');
+    leftBut = document.querySelector('.main .wrap .left .slider .left');
+    rightBut.onclick = function(){
+        if(parseInt(slideWrapper.style.left) + parseInt(slideWrapper.offsetWidth) < windowWidth) {
+            slideWrapper.style.left = '0px';
+
+        }
+        else {
+            slideWrapper.style.left = parseInt(slideWrapper.style.left) -step + 'px';
+
+        }
+    }
+    leftBut.onclick = function(){
+        if(parseInt(slideWrapper.style.left) == 0) {
+            slideWrapper.style.left = - Math.ceil((parseInt(slideWrapper.offsetWidth) - parseInt(windowWidth))/step) * step + 'px';
+        }
+        else {
+            slideWrapper.style.left = parseInt(slideWrapper.style.left) + step + 'px';
+        }
+    }
+    document.querySelector('.main .wrap .left .slider').style.minHeight=parseInt(slideWrapper.offsetHeight)+'px';
+}
